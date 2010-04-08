4830c8abd64e5b08e3f134feec0e1f51f3db9ed4
Steve Frécinaux <code@istique.net>
2010-04-08 22:26:37 +0200

    Actually set the variables.
    When doing a query like "SELECT a, b FROM table", a A and a B variable
    will be set in the channel.

--- query.c
+++ query.c
@@ -1,6 +1,7 @@
 #include <asterisk.h>
 #include <asterisk/linkedlists.h>
 #include <asterisk/app.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <asterisk/file.h>
 #include <asterisk/logger.h>
@@ -15,12 +16,35 @@
  *****************************************************************************/
 
 static void
-set_vars (MYSQL_RES *result)
+prepare_var_name (const char *field_name, char *var_name, unsigned length)
+{
+  unsigned i = 0;
+  const char *f = field_name;
+  char *v = var_name;
+
+  while (++i < length && *f != '\0')
+    {
+      if (!isascii (*f) || !isalnum (*f))
+        *v = '_';
+      else if (islower (*f))
+        *v = toupper (*f);
+      else
+        *v = *f;
+      ++f;
+      ++v;
+    }
+  *v = '\0';
+}
+
+static void
+set_vars (struct ast_channel *chan,
+          MYSQL_RES *result)
 {
   unsigned int num_fields;
   MYSQL_FIELD *fields;
   MYSQL_ROW row;
   unsigned i;
+  char var_name[20];
 
   num_fields = mysql_num_fields (result);
   fields = mysql_fetch_fields (result);
@@ -28,8 +52,11 @@ set_vars (MYSQL_RES *result)
 
   for (i = 0; i < num_fields; i++)
     {
+      prepare_var_name (fields[i].name, var_name, sizeof (var_name));
+      pbx_builtin_setvar_helper (chan, var_name, row[i] ? row[i] : "");
+
       if (option_verbose > 2)
-        ast_verbose (VERBOSE_PREFIX_2 "set_vars: %s = %s\n", fields[i].name, row[i] ? row[i] : "NULL");
+        ast_verbose (VERBOSE_PREFIX_2 "set_vars: %s = %s\n", var_name, row[i] ? row[i] : "");
     }
 }
 
@@ -54,7 +81,7 @@ app_my_select_exec (struct ast_channel *chan,
 
   /* Fetch the result and set the variables. */
   if (num_rows >= 1)
-    set_vars (result);
+    set_vars (chan, result);
 
   /* Discard the eventual remaining rows */
   if (num_rows > 1)
